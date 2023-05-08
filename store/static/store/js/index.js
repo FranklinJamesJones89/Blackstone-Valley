43e36a4aebc444c56da4086b4b4ae0a1e88476e0
FranklinJamesJones89 <fjones89@icloud.com>
2023-05-08 13:44:43 -0400

    Add footer

--- store/static/store/js/index.js
+++ store/static/store/js/index.js
@@ -1,14 +1,31 @@
+//window.onload = function() {
+//  let modal = document.getElementById("myModal");
+//  modal.style.display = "block";
+//  let yesBtn = document.getElementById("yesBtn");
+//  let noBtn = document.getElementById("noBtn");
+//  yesBtn.onclick = function() {
+//    modal.style.display = "none";
+//  };
+//  noBtn.onclick = function() {
+//    /*window.location.href = "https://example.com";*/
+//		let restrict = document.querySelector('.restrict');
+//		restrict.style.display = 'block';
+//  };
+//};
+
 window.onload = function() {
-  let modal = document.getElementById("myModal");
-  modal.style.display = "block";
-  let yesBtn = document.getElementById("yesBtn");
-  let noBtn = document.getElementById("noBtn");
-  yesBtn.onclick = function() {
-    modal.style.display = "none";
-  };
-  noBtn.onclick = function() {
-    /*window.location.href = "https://example.com";*/
-		let restrict = document.querySelector('.restrict');
-		restrict.style.display = 'block';
-  };
+	let modal = $('#myModal');
+	modal.css('display', 'block');
+
+	let yesBtn = $('#yesBtn');
+	let noBtn = $('#noBtn');
+
+	yesBtn.click(function() {
+		modal.css('display', 'none');	
+	});
+
+	noBtn.click(function() {
+		let restrict = $('.restrict');
+		restrict.css('display', 'block');
+	});
 };
